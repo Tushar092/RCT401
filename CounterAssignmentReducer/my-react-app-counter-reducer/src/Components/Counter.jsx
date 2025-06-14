3cc082520d1168657fa1fc6a541bb9747f58fc51
Ice_IQ92 <115460900+Tushar092@users.noreply.github.com>
2025-06-14 10:59:13 +0530

    Counter Reducer Application

---
+++ CounterAssignmentReducer/my-react-app-counter-reducer/src/Components/Counter.jsx
@@ -0,0 +1,32 @@
+import React, { useReducer } from "react";
+
+const initialState = {
+  count: 0,
+};
+
+function counterReducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1 };
+    case "DECREMENT":
+      return { count: state.count - 1 };
+    case "RESET":
+      return { count: 0 };
+    default:
+      return state;
+  }
+}
+
+function Counter() {
+  const [state, dispatch] = useReducer(counterReducer, initialState);
+  return (
+    <div>
+      <p>Counter {state.count}</p>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>Decrement</button>
+      <button onClick={() => dispatch({ type: "RESET" })}>Reset</button>
+    </div>
+  );
+}
+
+export default Counter;
